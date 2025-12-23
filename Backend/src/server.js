dc77b635771910545609a53e5225c630cb171794
Shantanu-Kulkarni1229 <shantanukulkarni1229@gmail.com>
2025-12-23 12:28:52 +0530

    feat: integrate Inngest for user synchronization and management
    - Added Inngest dependency and configured functions for user creation and deletion.
    - Updated environment variables to include Inngest and Stream API keys.
    - Implemented user model with Mongoose for database interactions.
    - Enhanced server setup with CORS support and middleware for JSON parsing.
    - Removed unused React SVG asset from the frontend.

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,27 +1,41 @@
 import express from 'express';
-import {ENV} from "./lib/env.js";
+import { ENV } from "./lib/env.js";
 import { connectDB } from './lib/db.js';
+import cors from 'cors';
+import {serve} from 'inngest/express';
+import { inngest } from './lib/inngest.js';
 
 const app = express();
 
-app.get("/" , (req , res) => {
-res.status(200).json({message: "Server is running"});
+//Middlewares
+app.use(express.json());
+// CREDENTIAL TRUE = ALLOWS COOKIES TO BE SENT
+app.use(cors({origin: ENV.CLIENT_URL , credentials: true}));
+
+app.use("/api/inngest" , serve({
+    client: inngest,
+    functions
+}));
+
+//Routes
+app.get("/", (req, res) => {
+    res.status(200).json({ message: "Server is running" });
 })
 
 
 
-const startServer = async() => {
-try {
-    await connectDB();
-    app.listen(ENV.PORT , () => {
-    console.log(`Server is running on port ${ENV.PORT}`);
-    
-});
-    
-} catch (error) {
-    console.error(`Error Starting Server: ${error.message}`);
-    process.exit(1);
-}
+const startServer = async () => {
+    try {
+        await connectDB();
+        app.listen(ENV.PORT, () => {
+            console.log(`Server is running on port ${ENV.PORT}`);
+
+        });
+
+    } catch (error) {
+        console.error(`Error Starting Server: ${error.message}`);
+        process.exit(1);
+    }
 
 }
 
