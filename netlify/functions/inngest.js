a0055a6fb31b0d51a114e974b8304ffa36a99dd5
Shantanu-Kulkarni1229 <shantanukulkarni1229@gmail.com>
2025-12-23 16:17:12 +0530

    Fix: make Netlify Inngest function fully CommonJS

--- netlify/functions/inngest.js
+++ netlify/functions/inngest.js
@@ -1,5 +1,5 @@
 const { serve } = require("inngest/netlify");
-const { inngest, functions } = require("../../src/lib/inngest.cjs");
+const { inngest, functions } = require("../../Backend/src/lib/inngest.cjs");
 
 exports.handler = serve({
   client: inngest,
